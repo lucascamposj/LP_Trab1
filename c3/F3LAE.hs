ed556a34039117a707d13c354b4de3b883b3a467
lucascamposj <lucascj29@gmail.com>
2017-09-21 11:45:51 -0300

    Lógica completa da Q1 realizada
    Lógica completa, porem falta realizar testes.

--- c3/F3LAE.hs
+++ c3/F3LAE.hs
@@ -33,38 +33,38 @@ interp (Sub l r) subs decs     = binOperation (-) l r subs decs
 
 interp (Let x e1 e2) subs decs = interp e2 ((x,exp):subs) decs 
   where
-	exp    = interp e1 subs decs
-	
-interp (Ref v) subs _			= 
-  let r = lookupC v subs
+    exp = interp e1 subs decs
+
+interp (Ref v) subs _ =
+  let r = lookupV v subs
   in case r of
       (Nothing) -> error "Variable not declared" -- Caso não ache a variavel na liste 
-      (Just f ) -> f                                    --
+      (Just v ) -> v                                    --
 
-interp (Lambda v b) subs decs  = Closure v exp subs
+interp (Lambda v b) subs decs  = Closure v b subs
 
--- interp (LambdaApp e1 e2) subs decs = interp e2 subs decs
---   where 
--- 	(Lambda x e) = interp e1 subs decs
--- 	sub          = (subst x e2 e decs)  
-interp (LambdaApp e1 e2) subs decs = interp e ((foa,w) : decsclo) -- Terminada ( falta testar ) 
+interp (LambdaApp e1 e2) subs decs = interp e ((foa,w) : decsclo) decs -- Terminada ( falta testar ) 
   where
     w = interp e2 subs decs
     (Closure foa e decsclo) = interp e1 subs decs
 
-interp (App n e) subs decs =
-  let f = lookupF n decs
-  in case f of
-      (Nothing) -> error "Function not declared"
-      (Just (FunDec m a b)) -> interp (subst a e b decs) decs  
+interp (App n e) subs decs = interp expf subs2 decs
+  where
+    (FunDec name fa expf) = let f = lookupF n decs
+                              in case f of
+                                  (Nothing) -> error "Function not declared"
+                                  (Just fundec) -> fundec
+    l1 = interp e subs decs
+    subs2 = ((fa,l1):subs)
+
 
 -- ESTRUCTURES
 
 binOperation :: (Integer -> Integer -> Integer) -> Exp -> Exp -> DefrdSub -> [FunDec] -> Value 
-binOperation op e1 e2 subs decs = interp Num (op n1 n2)  
+binOperation op e1 e2 subs decs = interp (Num (op n1 n2)) subs decs  
  where 
-  (Num n1) = interp e1 subs decs
-  (Num n2) = interp e2 subs decs
+  (NumValue n1) = interp e1 subs decs
+  (NumValue n2) = interp e2 subs decs
 
 lookupF :: Name -> [FunDec] -> Maybe FunDec
 lookupF _ [] = Nothing 
@@ -72,13 +72,13 @@ lookupF name (fun@(FunDec n a b):fs)
   | name == n = Just fun
   | otherwise = lookupF name fs 
 
-lookupC :: Name -> DefrdSub -> Maybe Value -- Retornar somente o valor
-lookupC _ [] = Nothing 
-lookupC name (fun@(Closure func x s):cs)
-  | name == func = Just fun
+
+lookupV :: Name -> DefrdSub -> Maybe Value -- Retornar somente o valor
+lookupV _ [] = Nothing 
+lookupV name ((id, value):cs)
+  | name == id = Just value
   | otherwise = lookup name cs
-lookupC name (NumValue value:cs)
-  | name == 
+
 
 
 
