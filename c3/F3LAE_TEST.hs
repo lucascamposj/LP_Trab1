debb375486a8b6fa5379d9759a8b23521eac5d9f
lucascamposj <lucascj29@gmail.com>
2017-09-21 22:28:39 -0300

    Correção do módulo de teste da Q1
    Teste 2 com erro de parser, verificar parênteses para encontrar erros.

--- c3/F3LAE_TEST.hs
+++ c3/F3LAE_TEST.hs
@@ -7,13 +7,13 @@ import F3LAE
 parse :: String -> Exp
 parse s = read s 
 
--- some HUnit test cases to better understand the calc semantics
+-- Testes da questão 1
 
 exp1, exp2 :: String 
 exp1 = "Num 8"
-exp2 = "(Let (Ref x) (Num 3) (Let (Ref f) (Lambda (Ref y) ((Ref y) + (Ref x)) (Let (Ref x) (Num 5) (LambdaApp (Ref f) (Num 4) ))) [] [])" 
+exp2 = "(Let (Ref x) (Num 3) (Let (Ref f) (Lambda (Ref y) ((Ref y) + (Ref x))) (Let (Ref x) (Num 5) (LambdaApp (Ref f) (Num 4) ))))" 
 
-tc1 = TestCase (assertEqual "tc01" (interp (parse exp1)) (Num 8))
+tc1 = TestCase (assertEqual "tc01" (interp (parse exp1) [] []) (NumValue 8))
 
-tc2 = TestCase (assertEqual "tc02" (interp (parse exp2)) (Num 8)) 
+tc2 = TestCase (assertEqual "tc02" (interp (parse exp2) [] []) (NumValue 8)) 
 
