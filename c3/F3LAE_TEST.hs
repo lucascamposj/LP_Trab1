4526b676511021d7f6dbb4f2a0318e42272d5906
lucascamposj <lucascj29@gmail.com>
2017-09-22 12:15:42 -0300

    Adição de testes.
    Adição de testes para a Q1. Testes funcionando e com resultados positivos.

--- c3/F3LAE_TEST.hs
+++ c3/F3LAE_TEST.hs
@@ -9,11 +9,28 @@ parse s = read s
 
 -- Testes da questão 1
 
-exp1, exp2 :: String 
-exp1 = "Num 8"
-exp2 = "(Let (Ref x) (Num 3) (Let (Ref f) (Lambda (Ref y) ((Ref y) + (Ref x))) (Let (Ref x) (Num 5) (LambdaApp (Ref f) (Num 4) ))))" 
+-- exp1, exp2 :: String 
+exp1 = Num 8
+exp2 = Add (Num 3) (Num 4)
+exp3 = Sub (Num 3) (Num 5)
+exp4 = Ref "x"
+exp5 = App "funcao" (Num 4) 
+exp6 = Let ("x") (Num 3) (Num 4)
+exp7 = Let ("y") (Num 4) (Add (Ref "y") (Num 4))
+exp8 = Let ("x") (Num 3) (Let ("f") (Lambda ("y") (Add (Ref "y") (Ref "x"))) (Let ("x") (Num 5) (LambdaApp (Ref "f") (Num 4))))
 
-tc1 = TestCase (assertEqual "tc01" (interp (parse exp1) [] []) (NumValue 8))
+tc1 = TestCase (assertEqual "tc01" (interp (exp1) [] []) (NumValue 8))
 
-tc2 = TestCase (assertEqual "tc02" (interp (parse exp2) [] []) (NumValue 8)) 
+tc2 = TestCase (assertEqual "tc02" (interp (exp2) [] []) (NumValue 7))
 
+tc3 = TestCase (assertEqual "tc03" (interp (exp3) [] []) (NumValue (-2)))
+
+tc4 = TestCase (assertEqual "tc04" (interp (exp4) [("x",NumValue 3)] []) (NumValue 3))
+
+tc5 = TestCase (assertEqual "tc05" (interp (exp5) [] [(FunDec "funcao" "x" (Add (Ref "x") (Num 4)))]) (NumValue 8))
+
+tc6 = TestCase (assertEqual "tc06" (interp (exp6) [] []) (NumValue 4))
+
+tc7 = TestCase (assertEqual "tc07" (interp (exp7) [] []) (NumValue 8))
+
+tc8 = TestCase (assertEqual "tc08" (interp (exp8) [] []) (NumValue 7))
