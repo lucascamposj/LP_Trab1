e6f7dad19184f3faacae5a4a19128d3e6ed2fa07
Patrick Beal <patrickvrb@gmail.com>
2017-09-24 00:53:28 -0300

    Acrescentado um teste no F5LAE_TEST.hs

--- c3/F5LAE_TEST.hs
+++ c3/F5LAE_TEST.hs
@@ -13,4 +13,8 @@ parse s = read s
 
 exp13 = Let ("x") (Num 3) (Let ("f") (Lambda ("y") (Add (Ref "y") (Ref "x"))) (Let ("x") (Num 5) (LambdaApp (Ref "f") (Num 4))))
 
+exp14 = Let ("x") (Num 4) (Let ("f") (Lambda ("z") (Sub (Ref "x") (Ref "z"))) (Let ("z") (Num 5) (LambdaApp (Ref "f") (Num 5))))
+
 tc13  = TestCase (assertEqual "tc13" (interp (exp13) [] []) (NumValue 9))
+
+tc14  = TestCase (assertEqual "tc14" (interp (exp14) [] []) (NumValue 10))
