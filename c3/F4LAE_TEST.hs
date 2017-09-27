046b3aa9d61bcbab9020424c2e293ed09e9c3c78
lucascamposj <lucascj29@gmail.com>
2017-09-27 10:31:20 -0300

    Alteração dos testes para strings e uso de listas de testes

--- c3/F4LAE_TEST.hs
+++ c3/F4LAE_TEST.hs
@@ -7,18 +7,18 @@ import F4LAE
 parse :: String -> Exp
 parse s = read s 
 
--- Testes da questão 2
 
--- exp1, exp2 :: String 
-
-exp11 = IfZero (Num 0) (Num 3) (Num 4)
-exp12 = App "test12" [(Num 2), (Num 3)]
+exp11 = "IfZero (Num 0) (Num 3) (Num 4)"
+exp12 = "App \"test12\" [(Num 2), (Num 3)]"
 
 
 
 
 
-tc11  = TestCase (assertEqual "tc11" (interp (exp11) [] []) (NumValue 3))
+tc11  = TestCase (assertEqual "tc11" (interp (parse exp11) [] []) (NumValue 3))
 
-tc12  = TestCase (assertEqual "tc12" (interp (exp12) [] [(FunDec "test12" ["x","y"] (Add (Ref "x") (Ref "y")))]) (NumValue 5))
+tc12  = TestCase (assertEqual "tc12" (interp (parse exp12) [] [(FunDec "test12" ["x","y"] (Add (Ref "x") (Ref "y")))]) (NumValue 5))
 
+tests = TestList [ TestLabel "tc11" tc11, 
+                    TestLabel "tc12" tc12
+                 ]
--- c3/F5LAE_TEST.hs
+++ c3/F5LAE_TEST.hs
@@ -7,14 +7,13 @@ import F5LAE
 parse :: String -> Exp
 parse s = read s 
 
--- Testes da questão 2
 
--- exp1, exp2 :: String 
+exp13 = "Let (\"x\") (Num 3) (Let (\"f\") (Lambda (\"y\") (Add (Ref \"y\") (Ref \"x\"))) (Let (\"x\") (Num 5) (LambdaApp (Ref \"f\") (Num 4))))"
 
-exp13 = Let ("x") (Num 3) (Let ("f") (Lambda ("y") (Add (Ref "y") (Ref "x"))) (Let ("x") (Num 5) (LambdaApp (Ref "f") (Num 4))))
+-- exp14 = "Let (\"x\") (Num 4) (Let (\"f\") (Lambda (\"z\") (Sub (Ref \"x\") (Ref \"z\"))) (Let (\"z\") (Num 5) (LambdaApp (Ref \"f\") (Num 5))))"
 
-exp14 = Let ("x") (Num 4) (Let ("f") (Lambda ("z") (Sub (Ref "x") (Ref "z"))) (Let ("z") (Num 5) (LambdaApp (Ref "f") (Num 5))))
+tc13  = TestCase (assertEqual "tc13" (interp (parse exp13) [] []) (NumValue 9))
 
-tc13  = TestCase (assertEqual "tc13" (interp (exp13) [] []) (NumValue 9))
+-- tc14  = TestCase (assertEqual "tc14" (interp (parse exp14) [] []) (NumValue 10))
 
-tc14  = TestCase (assertEqual "tc14" (interp (exp14) [] []) (NumValue 10))
+tests = TestList [ TestLabel "tc13" tc13]
